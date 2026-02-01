ef02232a6c1e3e34a870f7b636a45bb0fe7e51aa
Scottie Murphy <smurphy77@proton.me>
2026-02-01 15:40:58 -0500

    this part 2 is too slow

---
+++ part2.hs
@@ -0,0 +1,92 @@
+import Data.List
+
+-- utility functions
+
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = [thisPart] ++ split c restOfString where
+  (thisPart,tooLong) = break (==c) s
+  restOfString = case tooLong of
+    [] -> ""
+    _ -> tail tooLong
+
+prettyList :: (Show a) => [a] -> String
+prettyList [] = ""
+prettyList (x:xs) = (show x) ++ "\n" ++ prettyList xs
+
+pp :: (Show a) => [a] -> IO ()
+pp xs = putStrLn $ prettyList xs
+
+-- AoC 2025 - Day 11 - Part One - solution code
+
+data Node = Node String [String]
+  deriving (Eq,Show)
+
+type Path = String
+
+processLine :: String -> Node
+processLine s = newNode where
+  myTokens = words s
+  myName = (init . head) myTokens
+  myOutputs = tail myTokens
+  newNode = Node myName myOutputs
+
+justNames :: [Node] -> [String]
+justNames [] = []
+justNames ((Node name outs):ns) = [name] ++ justNames ns
+
+justOuts :: [Node] -> [String]
+justOuts [] = []
+justOuts ((Node name outs):ns) = union outs (justOuts ns)
+
+originNode :: [Node] -> String
+originNode ns = head $ (justNames ns) \\ (justOuts ns)
+
+-- fortunately, both the example data set and the big data set
+-- have one origin node and only one dead-end node ("out")
+
+nodeToPaths :: Node -> [String]
+nodeToPaths (Node name kids) = map (\x -> name ++ " " ++ x) kids
+
+expandNode :: String -> [Node] -> [String]
+expandNode _ [] = []
+expandNode target ((Node name kids):ns)
+  | target == name = nodeToPaths (Node name kids)
+  | otherwise = expandNode target ns
+
+isComplete :: Path -> String -> Bool
+isComplete thisPath target = last (words thisPath) == target
+
+allButLast :: Path -> Path
+allButLast p = foldl1 (\x y -> x ++ " " ++ y) (init $ words p)
+
+growPath :: Path -> String -> [Node] -> [Path]
+growPath p target myNodes
+  | isComplete p target = [p]
+  | otherwise = map (\x -> allButLast p ++ " " ++ x) (expandNode (last $ words p) myNodes) 
+
+recursePaths :: [Path] -> String -> [Node] -> [Path]
+recursePaths ps target myNodes
+  | all (\x -> isComplete x target) ps = ps
+  | otherwise = recursePaths (concatMap (\x -> growPath x target myNodes) ps) target myNodes
+
+justDacFft :: Path -> Bool
+justDacFft p = (elem "dac" $ wordList) && (elem "fft" $ wordList) where
+  wordList = words p
+
+-- recursePathsIO :: [Path] -> [Node] -> IO [Path]
+-- recursePathsIO ps myNodes
+--   | all isComplete ps = return ps
+--   | otherwise = do
+--       let nextLayer = concatMap (\x -> growPath x myNodes) ps
+--       pp nextLayer
+--       recursePathsIO (nextLayer) myNodes
+
+pathsBetween :: String -> String -> [Node] -> [Path]
+pathsBetween x y myNodes = recursePaths (expandNode x myNodes) y myNodes
+
+main = do
+  myFile <- readFile "data11.txt"
+  let myData = map processLine $ lines myFile
+  
+  pp $ pathsBetween "svr" "fft" myData
