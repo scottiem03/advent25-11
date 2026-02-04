a361ccc4fa7ed8f12fb308b68283993c17ac3a21
Scottie Murphy <smurphy77@proton.me>
2026-02-03 20:58:29 -0500

    works!

--- part2.hs
+++ part2.hs
@@ -17,82 +17,122 @@ prettyList (x:xs) = (show x) ++ "\n" ++ prettyList xs
 pp :: (Show a) => [a] -> IO ()
 pp xs = putStrLn $ prettyList xs
 
-lookupSrt :: (Ord a) => a -> [(a,b)] -> b
-lookupSrt target list
-  | target == midPoint = midCargo
-  | target < midPoint = lookupSrt target firstHalf
-  | target > midPoint = lookupSrt target secondHalf
+lookupKids :: (Ord a,Eq b,Eq c) => a -> [(a,b,c)] -> Maybe b
+lookupKids target list
+  | target == midPoint = Just midCargo
+  | firstHalf == [] = Nothing
+  | target < midPoint = lookupKids target firstHalf
+  | target > midPoint = lookupKids target secondHalf
   where
     midIndex = div (length list) 2
-    midPoint = fst $ list !! midIndex
-    midCargo = snd $ list !! midIndex
+    (midPoint,midCargo,midVisited) = list !! midIndex
     (firstHalf,secondHalf) = splitAt midIndex list
-    
 
--- AoC 2025 - Day 11 - Part One - solution code
+lookupVisit :: (Ord a,Eq b,Eq c) => a -> [(a,b,c)] -> Maybe c
+lookupVisit target list
+  | target == midPoint = Just midVisited
+  | firstHalf == [] = Nothing
+  | target < midPoint = lookupVisit target firstHalf
+  | target > midPoint = lookupVisit target secondHalf
+  where
+    midIndex = div (length list) 2
+    (midPoint,midCargo,midVisited) = list !! midIndex
+    (firstHalf,secondHalf) = splitAt midIndex list
 
-data Graph = Node String [Graph] | Out
+first :: (a,b,c) -> a
+first (x,_,_) = x
+
+second :: (a,b,c) -> b
+second (_,x,_) = x
+
+third :: (a,b,c) -> c
+third (_,_,x) = x
+
+fromMaybeK :: Maybe [a] -> [a]
+fromMaybeK Nothing = []
+fromMaybeK (Just x) = x
+
+fromMaybeV :: Maybe Visit -> Visit
+fromMaybeV Nothing = Paths 0
+fromMaybeV (Just x) = x
+
+-- graph-specific code
+
+type Node = (String,[String],Visit)
+data Visit = Unex | Paths Int
   deriving (Eq,Show)
 
-processLine :: String -> (String,[String])
-processLine s = (name,kids) where
+unexplored :: String -> [Node] -> Bool
+unexplored name myNodes = (getVisit name myNodes) == Unex
+
+explored :: String -> [Node] -> Bool
+explored name myNodes = (getVisit name myNodes) /= Unex
+
+sumNodes :: [String] -> [Node] -> Int
+sumNodes [] _ = 0
+sumNodes (k:ks) myNodes
+  | myVisit == Unex = error "can't sum unexplored node"
+  | otherwise = thisNum + sumNodes ks myNodes
+  where
+    myVisit = getVisit k myNodes
+    thisNum = fromPaths myVisit
+
+sumKids :: String -> [Node] -> Int
+sumKids name myNodes = sumNodes (getKids name myNodes) myNodes
+
+fromPaths :: Visit -> Int
+fromPaths Unex = 0
+fromPaths (Paths n) = n
+
+processLine :: String -> (String,[String],Visit)
+processLine s = (name,kids,visited) where
   myTokens = words s
   name = (init . head) myTokens
   kids = tail myTokens
+  visited = Unex
 
-findStrKids :: String -> [(String,[String])] -> [String]
-findStrKids target list = lookupSrt target list
+getKids :: String -> [Node] -> [String]
+getKids target myNodes = fromMaybeK $ lookupKids target myNodes
 
-buildGraph :: Graph -> [(String,[String])] -> Graph
-buildGraph Out _ = Out
-buildGraph (Node name kids) myMap = Node name (map (\x -> buildGraph x myMap) newKids) where
-  kidNames = findStrKids name myMap
-  newKids = map (\x -> case x of "out" -> Out; _ -> Node x []) kidNames
+noKids :: String -> [Node] -> Bool
+noKids name myNodes = length (getKids name myNodes) == 0
 
-buildGraphTo :: String -> Graph -> [(String,[String])] -> Graph
-buildGraphTo _ Out _ = Out
-buildGraphTo target (Node name kids) _
-  | target == name = (Node name [])
-buildGraphTo target (Node name kids) myMap = Node name (map (\x -> buildGraphTo target x myMap) newKids) where
-  kidNames = findStrKids name myMap
-  newKids = map (\x -> case x of "out" -> Out; _ -> Node x []) kidNames
+getVisit :: String -> [Node] -> Visit
+getVisit target myNodes = fromMaybeV $ lookupVisit target myNodes
 
-cleanUp :: String -> Graph -> Graph
-cleanUp _ Out = Out
-cleanUp target (Node name kids) = Node name (filter (leadsTo target) kids)
+justUnexKids :: String -> [Node] -> [String]
+justUnexKids parent myNodes = filter (\x -> unexplored x myNodes ) candidates where
+  candidates = getKids parent myNodes
 
-leadsTo :: String -> Graph -> Bool
-leadsTo _ Out = False
-leadsTo target (Node name kids)
-  | target == name = True
-  | otherwise = foldl1 (||) (map (leadsTo target) kids)
+pathsBetween :: String -> String -> [Node] -> [Node]
+pathsBetween start finish myNodes
+-- if this is the finish node, Paths = 1
+  | start == finish = markPaths start 1 myNodes
+-- if this is the end of a branch, Paths = 0
+  | noKids start myNodes = markPaths start 0 myNodes
+-- if all the kids are explored, Paths = sum of the kids' Paths
+  | all (\x -> explored x myNodes) (getKids start myNodes) = markPaths start (sumKids start myNodes) myNodes
+-- otherwise, check one of the unexplored kids and try again with updated Graph
+  | otherwise = pathsBetween start finish (pathsBetween (head $ justUnexKids start myNodes) finish myNodes)
 
-getName :: Graph -> String
-getName Out = "out"
-getName (Node name kids) = name
+numPathsBetween :: String -> String -> [Node] -> Int
+numPathsBetween start finish myNodes = mySum where
+  mySum = fromPaths $ getVisit start $ pathsBetween start finish myNodes
 
-findKidByName :: String -> [Graph] -> Graph
-findKidByName _ [] = error "kid not found"
-findKidByName name (k:ks)
-  | getName k == name = k
-  | otherwise = findKidByName name ks
-
--- numPathsBetween :: String -> String -> [(String,[String])]
-
-pathsTo :: String -> Graph -> Int
-pathsTo _ Out = 0
-pathsTo target (Node name kids)
-  | target == name = 1
-  | otherwise = sum $ map (pathsTo target) kids
+markPaths :: String -> Int -> [Node] -> [Node]
+markPaths _ _ [] = []
+markPaths name value ((thisName,theseKids,thisVisit):ns)
+  | name == thisName = (thisName,theseKids,Paths value) : ns
+  | otherwise = (thisName,theseKids,thisVisit) : markPaths name value ns
 
 main = do
-  myFile <- readFile "exdata11.txt"
-  let myData = sort $ map processLine $ lines myFile
+  myFile <- readFile "data11.txt"
+  let myData = sortOn first $ (("out",[],Unex):(map processLine $ lines myFile))
 
-  pp myData
+  -- pp $ myData
 
-  let myGraph = buildGraph (Node "svr" []) myData
+  let factor1 = numPathsBetween "dac" "out" myData
+  let factor2 = numPathsBetween "fft" "dac" myData
+  let factor3 = numPathsBetween "svr" "fft" myData
 
-  print $ myGraph
-
-  print $ pathsTo "dac" myGraph
+  print $ factor1 * factor2 * factor3
